bf7518ff4df5a38eb07f7442645215fe213b46bf
Dmity Razdoburdin <d.razdoburdin@gmail.com>
2016-11-20 00:26:43 +0300

    main.cpp upgrade

--- sourses/main.cpp
+++ sourses/main.cpp
@@ -22,20 +22,30 @@ int main(int N_data, char** char_data)
 //	vec.write(data);
 
 //2. Calculating of N singular values.
-//	optimal* singular_vectors;
-//	int j;
-//	int N=1;
-//	singular_vectors=(optimal*) malloc (N*sizeof(optimal));
-//	for(j=0;j<N;j++)
-//	{
-//		singular_vectors[j]=optimal(data,bg,singular_vectors,j);
-//		singular_vectors[j].write_G(data,bg,j+1);
-//	}
+	optimal* singular_vectors;
+	int j;
+	int N=5;
+	singular_vectors=(optimal*) malloc (N*sizeof(optimal));
+	for(j=0;j<N;j++)
+	{
+		singular_vectors[j]=optimal(data,bg,singular_vectors,j);
+		singular_vectors[j].write_G(data,bg,j+1);
+	}
 
 //3. Calculating of G as function of kx.
-	double kx_min=-25*data.ky;
-	double kx_max=-24*data.ky;
-	G_kx(data,bg,kx_min,kx_max,0.5*data.ky);
+//	double kx_min=-25*data.ky;
+//	double kx_max=10*data.ky;
+//	G_kx(data,bg,kx_min,kx_max,0.5*data.ky);
+
+//4. Calculating G_max by using analytical expression for T_max. Not tested yet.
+//	double Re_05=2000;
+//	data.Topt=pow(Re_05,1.0/3.0)*pow(data.ky*0.5,-2.0/3.0)/data.q;
+//	data.kx=-data.ky*data.q*data.Topt;
+//	fprintf(stderr,"Topt=%.2lf\tkx=%.2lf\tbeta=%.1lf\n",data.Topt,data.kx,data.kx/data.ky);
+
+//5. Calculating maximal G over all kx. Not tested yet.
+//	G_T G(data, bg);
+//	G.write(data,bg);
 
 	return 0;
 }
